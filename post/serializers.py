6b367757324620e4165e18c3091eaab3c14fccba
barabarinov <barabarinov@gmail.com>
2024-11-11 17:03:15 +0200

    Added serializers to User, Post

---
+++ post/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from .models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'content', 'created_at', 'likes_count')
+        read_only_fields = ("id", 'author', "created_at", 'likes_count')
---
+++ user/serializers.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data: dict) -> User:
+        return get_user_model().objects.create_user(**validated_data)
