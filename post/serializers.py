dc61046c4a436713a5057121e59d37ad2a3c4aae
barabarinov <barabarinov@gmail.com>
2024-11-12 20:37:03 +0200

    Applied black code formatting

--- post/serializers.py
+++ post/serializers.py
@@ -6,5 +6,5 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id', 'author', 'content', 'created_at', 'likes_count')
-        read_only_fields = ("id", 'author', "created_at", 'likes_count')
+        fields = ("id", "author", "content", "created_at", "likes_count")
+        read_only_fields = ("id", "author", "created_at", "likes_count")
